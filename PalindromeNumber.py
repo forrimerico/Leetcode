18a43fc359219f31c880011545a99c640e7a6a42
forrimerico <forrimerico@gmail.com>
2018-01-31 14:25:29 +0800

    3

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        for i in range(len(str(x))):
+            if str(x)[i] == str(x)[-(i+1)]:
+                continue
+            else:
+                return False
+
+        return True
+a = Solution()
+print(a.isPalindrome(123321))
