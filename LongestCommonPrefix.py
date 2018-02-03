4febc45f653ad60e40b7315a70b207f82446d33b
forrimerico <forrimerico@gmail.com>
2018-02-03 20:48:33 +0800

    1

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -4,8 +4,27 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        for i in strs:
-            print(i)
+        result = ''
+        i = 0
+        while True:
+            if strs == []:
+                return result
+
+            if i == len(strs[0]):
+                return result
+
+            temp = strs[0][i]
+
+            for str in strs:
+                if i == len(str):
+                    return result
+
+                if str[i] == temp:
+                    continue
+                else:
+                    return result
+            result = result + temp
+            i = i+1
 
 a = Solution()
-a.longestCommonPrefix(['asefsda','fasefds'])
+print(a.longestCommonPrefix([]))
